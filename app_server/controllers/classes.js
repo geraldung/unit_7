f9a843f2f7716fbcc1ab1a4f9839b7220048d24d
Gerald Ung <geraldung2008@gmail.com>
2018-03-04 22:42:20 -0700

    minor changes made

--- app_server/controllers/classes.js
+++ app_server/controllers/classes.js
@@ -1,28 +1,6 @@
 /* GET 'home' page */
 module.exports.classList = function(req, res) {
-  res.render('class-list', {
-    title: 'Course Organizer',
-    pageHeader: {
-      title: 'Course Organizer',
-      strapline: 'Helping you get that 4.0!',
-    },
-    classes: [{
-      name: 'CSCI 400: Programming Languages',
-      address: 'Berthoud 243'
-    }, {
-      name: 'CSCI 440: Parallel Computing',
-      address: 'Marquez 236'
-    }, {
-      name: 'CSCI 446: Web Applications',
-      address: 'Online'
-    }, {
-      name: 'CSCI 473: Human Centered Robotics',
-      address: 'Marquez 328'
-    }, {
-      name: 'CSCI 498: Robot Planning and Manipulation',
-      address: 'CoorsTek 141'
-    }]
-  });
+  res.render('class-list', {title: 'Course Organizer'});
 };
 
 /* GET 'Class info' page */
