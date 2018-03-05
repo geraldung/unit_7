0967de359c5e6464c16d288fca82ef69ba124383
Gerald Ung <geraldung2008@gmail.com>
2018-03-04 22:30:37 -0700

    Hardcoded web app functionality

--- app_server/controllers/classes.js
+++ app_server/controllers/classes.js
@@ -1,14 +1,36 @@
 /* GET 'home' page */
 module.exports.classList = function(req, res) {
-res.render('drugs-list', { title: 'Home' });
+  res.render('class-list', {
+    title: 'Course Organizer',
+    pageHeader: {
+      title: 'Course Organizer',
+      strapline: 'Helping you get that 4.0!',
+    },
+    classes: [{
+      name: 'CSCI 400: Programming Languages',
+      address: 'Berthoud 243'
+    }, {
+      name: 'CSCI 440: Parallel Computing',
+      address: 'Marquez 236'
+    }, {
+      name: 'CSCI 446: Web Applications',
+      address: 'Online'
+    }, {
+      name: 'CSCI 473: Human Centered Robotics',
+      address: 'Marquez 328'
+    }, {
+      name: 'CSCI 498: Robot Planning and Manipulation',
+      address: 'CoorsTek 141'
+    }]
+  });
 };
 
-/* GET 'Location info' page */
+/* GET 'Class info' page */
 module.exports.classInfo = function(req, res) {
-res.render('index', { title: 'Class Information' });
+  res.render('class-info', { title: 'Class Information' });
 };
 
-/* GET 'Add review' page */
-module.exports.addMaterial = function(req, res) {
-res.render('index', { title: 'Add Class Material' });
+/* GET 'Add assignment' page */
+module.exports.addAssignment = function(req, res) {
+  res.render('add-assignment-form', { title: 'Add Assignment' });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var ctrlClasses = require('../controllers/classes');
-var ctrlOthers = require('../controllers/others');
 
 /* Classes pages */
 router.get('/', ctrlClasses.classList);
-router.get('/class', ctrlClasses.classInfo);
-router.get('/class/review/new', ctrlClasses.addMaterial);
+router.get('/classes', ctrlClasses.classInfo);
+router.get('/classes/add_assignment/new', ctrlClasses.addAssignment);
 
-/* Other pages */
-router.get('/about', ctrlOthers.about);
 module.exports = router;
